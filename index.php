2b2686b3ee0ecef9f43158d0f6649b53e5d9e5ac
Dario Miranda <netobis2015@gmail.com>
2024-07-03 21:13:41 -0300

    update

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <?php
+    
+    if(!isset($_SESSION['login'])){
+        include('login.php');
+    }else{
+        include('home.php');
+    }
+
+
+    ?>
+
+</body>
+</html>
